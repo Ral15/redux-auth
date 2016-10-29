653ccd0c852b20add7023c5eca0b8346edf391a0
Raul Mar <Ral15@Rauls-MacBook-Pro.local>
2016-10-29 16:49:02 -0500

    validation email

--- src/views/bootstrap/EmailSignInForm.js
+++ src/views/bootstrap/EmailSignInForm.js
@@ -6,6 +6,12 @@ import { Glyphicon } from "react-bootstrap";
 import { connect } from "react-redux";
 
 class EmailSignInForm extends React.Component {
+  getInitialState() {
+    return {
+      validUser: false,
+      validPass: false
+    }
+  }
   static propTypes = {
     endpoint: PropTypes.string,
     next: PropTypes.func,
@@ -34,6 +40,15 @@ class EmailSignInForm extends React.Component {
   }
 
   handleInput (key, val) {
+    if (key === "email") {
+      const reg = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+      if (reg.test(val)) setState({ validUser: true })
+      else setState({ validUser: false })
+    }
+    if (key == "password") {
+      if (val.length < 8) setState({ validPass: false })
+      else setState({ validPass: true })
+    }
     this.props.dispatch(emailSignInFormUpdate(this.getEndpoint(), key, val));
   }
 
@@ -48,7 +63,8 @@ class EmailSignInForm extends React.Component {
   render () {
     let disabled = (
       this.props.auth.getIn(["user", "isSignedIn"]) ||
-      this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "loading"])
+      this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "loading"]) ||
+      this.state.validUser || this.state.validPass
     );
 
     return (
@@ -56,34 +72,34 @@ class EmailSignInForm extends React.Component {
             onSubmit={this.handleSubmit.bind(this)}>
         <div className="col-md-10 col-md-offset-1">
           <Input type="text"
-                 groupClassName="email-sign-in-email"
-                 label="Correo electrónico"
-                 placeholder="sr.envio@mienvio.mx"
-                 disabled={disabled}
-                 value={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form", "email"])}
-                 errors={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "errors", "email"])}
-                 onChange={this.handleInput.bind(this, "email")}
-                 {...this.props.inputProps.email} />
+                groupClassName="email-sign-in-email"
+                label="Correo electrónico"
+                placeholder="sr.envio@mienvio.mx"
+                disabled={disabled}
+                value={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form", "email"])}
+                errors={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "errors", "email"])}
+                onChange={this.handleInput.bind(this, "email")}
+                {...this.props.inputProps.email} />
         </div>
         <div className="col-md-10 col-md-offset-1">
           <Input type="password"
-                 label="Contraseña"
-                 groupClassName="email-sign-in-password"
-                 placeholder="********"
-                 disabled={disabled}
-                 value={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form", "password"])}
-                 errors={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "errors", "password"])}
-                 onChange={this.handleInput.bind(this, "password")}
-                 {...this.props.inputProps.password} />
+                label="Contraseña"
+                groupClassName="email-sign-in-password"
+                placeholder="********"
+                disabled={disabled}
+                value={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form", "password"])}
+                errors={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "errors", "password"])}
+                onChange={this.handleInput.bind(this, "password")}
+                {...this.props.inputProps.password} />
         </div>
         <div className="col-md-10 col-md-offset-1">
           <ButtonLoader loading={this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "loading"])}
-                        type="submit"
-                        icon={<Glyphicon glyph="log-in" />}
-                        className='email-sign-in-submit btn-danger btn-block'
-                        disabled={disabled}
-                        onClick={this.handleSubmit.bind(this)}
-                        {...this.props.inputProps.submit}>
+                type="submit"
+                icon={<Glyphicon glyph="log-in" />}
+                className='email-sign-in-submit btn-danger btn-block'
+                disabled={disabled}
+                onClick={this.handleSubmit.bind(this)}
+                {...this.props.inputProps.submit}>
             Inicia sesión
           </ButtonLoader>
         </div>
