78f5f293debfd859f8e649159c09a5b7bbb7387a
Raul Mar <raulmar_94@hotmail.com>
2016-12-16 15:59:52 -0700

    restored prevous settings

--- src/utils/handle-fetch-response.js
+++ src/utils/handle-fetch-response.js
@@ -1,9 +1,8 @@
-export function parseResponse (response, cb = () => {}) {
+export function parseResponse (response) {
   let json = response.json();
   if (response.status >= 200 && response.status < 300) {
     return json;
   } else {
-  	cb()
     return json.then(err => Promise.reject(err));
   }
 }
