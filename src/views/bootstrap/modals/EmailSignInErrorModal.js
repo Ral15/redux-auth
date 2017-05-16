caacb0609e03f274e413e25b28ede8d77820e89c
Raul Mar <raulmar_94@hotmail.com>
2017-05-16 18:07:42 -0500

    error message fixed

--- src/views/bootstrap/modals/EmailSignInErrorModal.js
+++ src/views/bootstrap/modals/EmailSignInErrorModal.js
@@ -4,13 +4,14 @@ import Modal from "./Modal";
 
 class EmailSignInErrorModal extends React.Component {
   render () {
+    const msg = "Alguno de los datos es invalido!";
     return (
       <Modal
         show={this.props.show}
         containerClass="email-sign-in-error-modal"
         closeAction={hideEmailSignInErrorModal}
         title="Error al iniciar sesión"
-        errorAddr={["emailSignIn", "errors"]} />
+        errorAddr={msg} />
     );
   }
 }
